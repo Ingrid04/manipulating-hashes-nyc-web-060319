3b7bdd390c762df021a50eb982ecd5cc465979b5
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-31 11:42:07 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,11 +12,9 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-
-  #your code here
-
-
+     contacts.delete_if { |k,v| v == "strawberry" }      #your code here
+     
   #remember to return your newly altered contacts hash!
-  contacts
+   return contacts
 end
 
